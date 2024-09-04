af13855ca90fd21515e9f5d9725f51ee5777a511
sevakul16 <sevakul16@gmail.com>
2024-09-04 15:50:18 +0300

    Added timer and progress bar

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const ProgressBar = ({ timer }) => {
+  const [remainingTime, setRemainingTime] = useState(timer);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => {
+        prevRemainingTime - 10;
+      });
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress value={remainingTime} max={timer} />;
+};
+
+export default ProgressBar;
